19f56ae50c3a51123ee65e8e2183516f58153450
Hamza11067 <hamzakhalid1067@gmail.com>
2025-09-24 20:12:28 +0500

    Made port accessible for the replit app

--- server.js
+++ server.js
@@ -21,8 +21,10 @@ app.use("/", userRoutes);
 app.use("/", productRoutes);
 app.use("/", cartRoutes);
 
+const PORT = process.env.PORT || 3000;
+
 connectDB().then(() => {
-  app.listen(3000, () => {
-    console.log("Server is running on port 3000");
+  app.listen(PORT, () => {
+    console.log("Server is running on port " + PORT);
   });
 });
